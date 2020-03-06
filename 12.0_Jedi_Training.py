5f4fdb5be67b73bc5ebb8e2e7f92ad7afb9d5915
moritzemm <emmamor2323>
2020-03-06 13:37:24 -0600

    03/6/2020

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -4,23 +4,23 @@ Sign your name:___Emma_E_Moritz                                             ___
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+The center of the window
 2.) What are the variables dx and dy?
-
+the X an Y axis
 3.) How many pixels/sec does the ball move in the x-direction?
-
+60* whatever number  you put
 4.) How many pixels/sec does the ball move in the y-direction?
-
+60* whatever number you put
 5.) Which method is run 60 times/second?
-
+The movement of the ball in any direction
 6.) What does this code do?   self.dx *= -1
-
+move the ball to the left at 60 pixles per second
 7.) What does this code do?  self.pos_y += self.dy
-
+sets the ball to the opposite direction
 8.) What is the width of the window?
-
+640 pixles
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+that the ball wont go off the edge
 10.) What is this code checking? if self.pos_x < self.rad
-
+that the ball is not partially over the edge?
 '''
--- testing 12.py
+++ testing 12.py
@@ -1,4 +1,5 @@
 import arcade
+import random
 SW=640
 SH=480
 class Ball:
@@ -17,25 +18,38 @@ class Ball:
         self.pos_x+=self.dx
         self.pos_y+=self.dy
 
-        #bouncing the ball off the edges
+        #bouncing the ball off the edges direct tv style
         if self.pos_x<self.radius or self.pos_x>SW-self.radius:
             self.dx*=-1
         if self.pos_y<self.radius or self.pos_y>SH-self.radius:
             self.dy*=-1
+        #make it pacman so it gose from side threw to the other side
+        '''
+        if self.pos_x>SW+self.radius:
+            self.pos_x=-self.radius
+        '''
 
 class MyGame(arcade.Window):
     '''This is my first game class'''
     def __init__(self,width,height,title):
         super().__init__(width,height,title)
         arcade.set_background_color(arcade.color.BUBBLES)
-        self.ball=Ball(SW/2,SH/2,3,-2,15,arcade.color.AFRICAN_VIOLET)
+        self.ball_list=[]
+        for i in range(10):
+            self.dx = random.randint(-2, 2)
+            self.dy = random.randint(-2, 2)
+            self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            self.ball=Ball(SW/2,SH/2,self.dx,self.dy,15,self.color)
+            self.ball_list.append(self.ball)
 
     def on_draw(self):
         arcade.start_render()
-        self.ball.draw_ball()
+        for ball in self.ball_list:
+            ball.draw_ball()
 
     def on_update(self,dt):
-        self.ball.update_ball()
+        for ball in self.ball_list:
+            ball.update_ball()
 
 def main():
     window=MyGame(SW,SH,"Animation")
